839bbaed6b62cd977fe84896633968106dd3a3e7
ado11231 <adoalagic0@gmail.com>
2026-02-26 12:09:37 -0500

    finished loaders file

---
+++ core/loader.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+def load_image(path):
+    img = cv.imread(str(path))
+    if img is None:
+        raise FileNotFoundError("Could Not Find Image")
+    
+    img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    
+    return img_rgb
+
+
+
