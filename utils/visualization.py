d78b497f39ff525ad4ea43f089b7df9e548c02c5
ado11231 <adoalagic0@gmail.com>
2026-02-26 15:24:46 -0500

    finished visualization

---
+++ utils/visualization.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+def compare_image(image1, image2):
+    figure, axes = plt.subplots(1,2)
+
+    axes[0].imshow(image1)
+    axes[0].set_title('Original')
+    axes[0].axis("off")
+
+    axes[1].imshow(image2)
+    axes[1].set_title('New')
+    axes[1].axis("off")
+
+    plt.tight_layout()
+
+    plt.show()
